f17ea45cc6ecefb395b8a6bd01030e444184f032
Paulo Vieira <vieirajjr@gmail.com>
2022-09-18 12:28:13 -0300

    czxcz

--- src/components/Cards/Franquia/index.js
+++ src/components/Cards/Franquia/index.js
@@ -1,33 +1,43 @@
 import React from 'react';
-import '../style.css'
+import './styles.css'
 
 export default function FranquiaPage() {
+  function handleabolt(){
+    window.location.replace('../Sobre')
+  };
+  
+  function handlevisa(){
+    window.location.replace('../Visto');
+  }
+
+  function handlefacul(){
+    window.location.replace('../Faculdade');
+  }
+
+  function handlefranquia(){
+    window.location.replace('../Franquia');
+  }
+
+  function handleeducation(){
+    window.location.replace('../Educacao');
+  }
+
  return (
- 
-     <div className='divloggeral'>
+  <div className='divloggeral'>
   <div className='cardsinone'>
      <div className='topinit'>
+     <div className='topinit'>
        <div className='apart'>
-         <div className='btn btnmenu '>Sobre</div>
-         <div className='btn btnmenu '>Visto/Passaporte</div>
-         <div className='btn btnmenu '>Educação</div>
-         <div className='btn btnmenu '>Faculdades</div>
-         <div className='btn btnmenu '>Franquia</div>
+         <div className='btn btnmenu ' onClick={handleabolt}>Sobre</div>
+         <div className='btn btnmenu ' onClick={handlevisa}>Visto/Passaporte</div>
+         <div className='btn btnmenu ' onClick={handleeducation}>Educação</div>
+         <div className='btn btnmenu ' onClick={handlefacul}>Faculdades</div>
+         <div className='btn btnmenu ' onClick={handlefranquia}>Franquia</div>
        </div>
        <div className='bpart'></div>
      </div>
-     <div className='divoneinit'>
-     
-     </div>
   </div>
-  <div className='cardsign'>
-    <div className='divlogsup'>
-    <div className='divloglogo'>
-    </div>
-    </div>
-    <div className='divlogsub'>
-      
-    </div>       
+       
     {/*
     <PasswordForgetLink />
     <SignUpLink />*/}
--- src/components/Cards/Sobre/index.js
+++ src/components/Cards/Sobre/index.js
@@ -1,8 +1,48 @@
 import React from 'react';
-import'../style.css';
+import'./styles.css';
 
 export default function SobrePage() {
+  function handleabolt(){
+    window.location.replace('../Sobre')
+  };
+  
+  function handlevisa(){
+    window.location.replace('../Visto');
+  }
+
+  function handlefacul(){
+    window.location.replace('../Faculdade');
+  }
+
+  function handlefranquia(){
+    window.location.replace('../Franquia');
+  }
+
+  function handleeducation(){
+    window.location.replace('../Educacao');
+  }
+
  return (
-   <div>Sobre</div>
+  <div className='divloggeral'>
+  <div className='cardsinone'>
+     <div className='topinit'>
+     <div className='topinit'>
+       <div className='apart'>
+         <div className='btn btnmenu ' onClick={handleabolt}>Sobre</div>
+         <div className='btn btnmenu ' onClick={handlevisa}>Visto/Passaporte</div>
+         <div className='btn btnmenu ' onClick={handleeducation}>Educação</div>
+         <div className='btn btnmenu ' onClick={handlefacul}>Faculdades</div>
+         <div className='btn btnmenu ' onClick={handlefranquia}>Franquia</div>
+       </div>
+       <div className='bpart'></div>
+     </div>
+  </div>     
+    {/*
+    <PasswordForgetLink />
+    <SignUpLink />*/}
+   
+  </div>
+
+  </div>
   );
 }
